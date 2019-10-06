6c2fd0f285ecb17accbee6ea5f5f428003c3a9a6
Janis <janis@customprogrammingsolutions.com>
2019-10-06 21:27:16 +1100

    Format source code consistently

--- pySpeechRev.py
+++ pySpeechRev.py
@@ -18,55 +18,50 @@ from supplib import load_IR
 from supplib import shift
 
 # Reading input arguments
-in_folder=sys.argv[1]   # input folder
-out_folder=sys.argv[2]  # output folder
-list_file=sys.argv[3]   # list file ("wav_file IR_file")
+in_folder = sys.argv[1]  # input folder
+out_folder = sys.argv[2]  # output folder
+list_file = sys.argv[3]  # list file ("wav_file IR_file")
 
 
 # Read List file
-[list_sig,list_ir]=ReadList(list_file)
+[list_sig, list_ir] = ReadList(list_file)
 
 # Replicate input folder structure to output folder
-copy_folder(in_folder,out_folder)
+copy_folder(in_folder, out_folder)
 
 # Speech Data Reverberation Loop
-for i in range(len(list_sig)): 
- 
- # Open clean wav file
- #[fs, signal_clean] = wavfile.read(list_sig[i])
- [signal_clean, fs] = sf.read(list_sig[i])
+for i in range(len(list_sig)):
 
- signal_clean=signal_clean.astype(np.float64)
+    # Open clean wav file
+    # [fs, signal_clean] = wavfile.read(list_sig[i])
+    [signal_clean, fs] = sf.read(list_sig[i])
 
- # Signal normalization
- signal_clean=signal_clean/np.max(np.abs(signal_clean))
- 
- # Open Impulse Response (IR)
- IR=load_IR(list_ir[i])
+    signal_clean = signal_clean.astype(np.float64)
 
- # IR normalization
- IR=IR/np.abs(np.max(IR))
- p_max=np.argmax(np.abs(IR))
- 
- signal_rev=signal.fftconvolve(signal_clean, IR, mode='full')
+    # Signal normalization
+    signal_clean = signal_clean / np.max(np.abs(signal_clean))
 
- # Normalization
- signal_rev=signal_rev/np.max(np.abs(signal_rev))
- 
- # IR delay compensation
- signal_rev=shift(signal_rev, -p_max)
+    # Open Impulse Response (IR)
+    IR = load_IR(list_ir[i])
 
- # Cut reverberated signal (same length as clean sig)
- signal_rev=signal_rev[0:signal_clean.shape[0]]
- 
- # Save Reverberated Speech
- file_out=list_sig[i].replace(in_folder, out_folder)
- #wavfile.write(file_out,fs,signal_rev)
- sf.write(file_out, signal_rev, fs)
- 
- print("Done %s" % (file_out))
+    # IR normalization
+    IR = IR / np.abs(np.max(IR))
+    p_max = np.argmax(np.abs(IR))
 
+    signal_rev = signal.fftconvolve(signal_clean, IR, mode="full")
 
+    # Normalization
+    signal_rev = signal_rev / np.max(np.abs(signal_rev))
 
+    # IR delay compensation
+    signal_rev = shift(signal_rev, -p_max)
 
+    # Cut reverberated signal (same length as clean sig)
+    signal_rev = signal_rev[0 : signal_clean.shape[0]]
 
+    # Save Reverberated Speech
+    file_out = list_sig[i].replace(in_folder, out_folder)
+    # wavfile.write(file_out,fs,signal_rev)
+    sf.write(file_out, signal_rev, fs)
+
+    print("Done %s" % (file_out))
--- supplib.py
+++ supplib.py
@@ -6,6 +6,7 @@ import os
 import soundfile as sf
 import shutil
 
+
 def shift(xs, n):
     e = np.empty_like(xs)
     if n >= 0:
@@ -15,34 +16,35 @@ def shift(xs, n):
         e[n:] = 0.0
         e[:n] = xs[-n:]
     return e
-  
-def copy_folder(in_folder,out_folder):
- if not(os.path.isdir(out_folder)):
-  shutil.copytree(in_folder, out_folder, ignore=ig_f)
- 
- 
+
+
+def copy_folder(in_folder, out_folder):
+    if not (os.path.isdir(out_folder)):
+        shutil.copytree(in_folder, out_folder, ignore=ig_f)
+
+
 def ig_f(dir, files):
     return [f for f in files if os.path.isfile(os.path.join(dir, f))]
-  
+
 
 def load_IR(IR_file):
-  
- if ".mat" in IR_file:
-  IR = loadmat(IR_file, squeeze_me=True, struct_as_record=False)
-  IR=IR[IR.keys()[0]]
- else:
-  [IR, fs] = sf.read(IR_file)
-  
- return IR
+
+    if ".mat" in IR_file:
+        IR = loadmat(IR_file, squeeze_me=True, struct_as_record=False)
+        IR = IR[IR.keys()[0]]
+    else:
+        [IR, fs] = sf.read(IR_file)
+
+    return IR
 
 
 def ReadList(list_file):
- f=open(list_file,"r")
- lines=f.readlines()
- list_sig=[]
- list_ir=[]
- for x in lines:
-    list_sig.append(x.split(' ')[0])
-    list_ir.append(x.split(' ')[1].rstrip())
- f.close()
- return [list_sig,list_ir]
+    f = open(list_file, "r")
+    lines = f.readlines()
+    list_sig = []
+    list_ir = []
+    for x in lines:
+        list_sig.append(x.split(" ")[0])
+        list_ir.append(x.split(" ")[1].rstrip())
+    f.close()
+    return [list_sig, list_ir]
